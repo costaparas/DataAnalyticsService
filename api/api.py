e1af9f0faea5a4a1db9c15e460f65046ef4ea9b1
Ben Liew <ben.liew.99@gmail.com>
2018-10-07 16:05:58 +1100

    Removed default namespace

--- api/api.py
+++ api/api.py
@@ -3,8 +3,6 @@ from flask_restplus import Api, Resource
 
 app = Flask(__name__)
 api = Api(app,
-          # default="Movies",
-          # default_label="Movies and recommendations.",
           title="Movie Recommendation API",
           description="This API provides access to a database of movie titles, information about each title and movie recommendations.",
           version="1.0"
@@ -13,14 +11,21 @@ api.namespaces.clear()
 movies_ns = api.namespace("movies", description="")
 recommendations_ns = api.namespace("recommendations", description="")
 
+
 @movies_ns.route('/list')
 class Movies(Resource):
     def get(self):
         return {'movies': []}
 
+
 @recommendations_ns.route('/<string:movie_id>')
 class Recommendations(Resource):
     def get(self, movie_id):
-        return {'recommendations': []}
+        return {
+            'movie_id': movie_id,
+            'recommendations': []
+        }
+
+
 if __name__ == '__main__':
     app.run(debug=True)
