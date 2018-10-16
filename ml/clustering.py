6ce74ac8e18ca2390f70e0ae1ae67d108b97c9e6
Costa Paraskevopoulos <costa.paras1@gmail.com>
2018-10-16 22:59:42 +1100

    parameterize clustering for evaluation

--- ml/clustering.py
+++ ml/clustering.py
@@ -6,7 +6,7 @@
 import numpy as np
 from sklearn.cluster import KMeans, MeanShift, AgglomerativeClustering, Birch
 from sklearn.feature_extraction.text import CountVectorizer
-import sys, json, codecs, re, collections, random, pickle, sklearn.metrics, enum
+import sys, json, codecs, re, collections, random, pickle, enum, sklearn.metrics
 
 
 @enum.unique
@@ -22,88 +22,15 @@ class Model_Types(enum.Enum):
 
 debug = False
 save_clusters = False
-write_evaluation_metrics = False
-model_type = Model_Types.AGGLOMERATIVE_EUCLID
-
-
-def read_movie_file():
-    try:
-        with open('../data/movies-large.json', 'r', encoding='utf-8') as f:
-            return json.load(f)
-    except (PermissionError, OSError) as e:
-        print(str(e), file=sys.stderr)
-        sys.exit(1)
-
-
-def get_movies_dict(all_movies, n_movies=1000):
-    """
-    # use a random subset of movies
-    # keys = random.sample(list(all_movies), n_movies)
-    keys = list(all_movies)
-    movies = {}
-    for key in keys:
-        movies[key] = all_movies[key]
-
-    return movies, len(keys)
-    """
-    return all_movies, len(list(all_movies))
-
-
-def write_clusters(clusters, cluster_numbers):
-    if debug: print('Writing Clusters')
-    try:
-        with open('clusters.pk', 'wb') as f:
-            pickle.dump(clusters, f)
-        with open('cluster-numbers.pk', 'wb') as f:
-            pickle.dump(cluster_numbers, f)
-    except (PermissionError, OSError) as e:
-        print(str(e), file=sys.stderr)
-        sys.exit(1)
-
-
-def write_metrics(model_name, model, labels, clusters=0, width=30):
-    # Write evaluation metrics to a file, while preserving stats of other models
-    # We also preserve stats of the same model if it uses a different number of clusters
-    try:
-        with open('mlMetrics.txt', 'r', encoding='utf-8') as f:
-            data = f.readlines()
-    except FileNotFoundError:
-        with open('mlMetrics.txt', 'w', encoding='utf-8') as f:
-            data = ["{:<{width}} {:<{width}} {:<{width}} {:<{width}}\n".format("Model Name", "# Clusters",
-                                                                             "Silhouette Score", "Calinski-Hara Score",
-                                                                             width=width)]
-            f.write(data[0])
-
-
-    line_to_write = len(data)
-    for count, line in enumerate(data):
-        lline = line.split()
-        if lline[0] == model_name and lline[1] == str(clusters):
-            line_to_write = count
-            break
-
-    # These are heuristics to tell us how "good" our clustering is, how similar things in the cluster are
-    # We want to maximise these without getting our clusters so high that they become too small
-    silhouette_score = sklearn.metrics.silhouette_score(model, labels)
-    calinski_hara_score = sklearn.metrics.calinski_harabaz_score(model, labels)
-
-    if line_to_write == len(data):
-        data.append("{:<{width}} {:<{width}} {:<{width}} {:<{width}}\n".format(
-                model_name, clusters, silhouette_score, calinski_hara_score, width=width))
-    else:
-        data[line_to_write] = "{:<{width}} {:<{width}} {:<{width}} {:<{width}}\n".format(
-                model_name, clusters, silhouette_score, calinski_hara_score, width=width)
-
-    with open('mlMetrics.txt', 'w', encoding='utf-8') as f:
-        f.writelines(data)
+write_evaluation_metrics = True
 
 
 def main():
+    model_type, n_clusters, linkage = parse_args()
+
     # read movie data
     all_movies = read_movie_file()
     movies, n_movies = get_movies_dict(all_movies)
-
-    n_clusters = 2 * int(n_movies / 10)
 
     # extract only relevant movie data
     genres = []
@@ -154,18 +81,18 @@ def main():
         model = Birch(n_clusters=n_clusters).fit_predict(cluster_features)
     elif model_type == Model_Types.AGGLOMERATIVE:
         model = AgglomerativeClustering(n_clusters=n_clusters,
-                                        affinity="cosine", linkage="complete").fit_predict(cluster_features)
+                                        affinity="cosine", linkage=linkage).fit_predict(cluster_features)
 
     elif model_type == Model_Types.AGGLOMERATIVE_L1:
         model = AgglomerativeClustering(n_clusters=n_clusters,
-                                        affinity="l1", linkage="complete").fit_predict(cluster_features)
+                                        affinity="l1", linkage=linkage).fit_predict(cluster_features)
 
     elif model_type == Model_Types.AGGLOMERATIVE_L2:
         model = AgglomerativeClustering(n_clusters=n_clusters,
-                                        affinity="l2", linkage="complete").fit_predict(cluster_features)
+                                        affinity="l2", linkage=linkage).fit_predict(cluster_features)
     elif model_type == Model_Types.AGGLOMERATIVE_MAN:
         model = AgglomerativeClustering(n_clusters=n_clusters,
-                                        affinity="manhattan", linkage="complete").fit_predict(cluster_features)
+                                        affinity="manhattan", linkage=linkage).fit_predict(cluster_features)
     elif model_type == Model_Types.AGGLOMERATIVE_EUCLID:
         model = AgglomerativeClustering(n_clusters=n_clusters).fit_predict(cluster_features)
 
@@ -185,10 +112,129 @@ def main():
     # Write out evaluation metrics
     if write_evaluation_metrics: write_metrics(model_type.name, cluster_features, model, n_clusters)
 
+def parse_args():
+    # too hard with argparse, so old-fashioned manual parsing
+    if len(sys.argv) < 2:
+        print('Usage: %s MODEL [N_CLUSTERS] [LINKAGE]' %sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    model_type = ''
+    n_clusters = 0
+    linkage = ''
+
+    # choose ml model
+    found_model = False
+    for m in list(Model_Types):
+       model = str(m)[str(m).index('.')+1:]
+       if model == sys.argv[1]:
+           model_type = m
+           found_model = True
+           break
+
+    if not found_model:
+        print('%s: invalid model type' %sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    if sys.argv[1] == 'MEANSHIFT' and len(sys.argv) > 2:
+        print('%s: unexpected trailing arguments' %sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    if sys.argv[1] != 'MEANSHIFT' and len(sys.argv) < 3:
+        print('%s: expecting number of clusters' %sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    elif sys.argv[1] != 'MEANSHIFT':
+        try:
+            n_clusters = int(sys.argv[2])
+        except ValueError:
+            print('%s: expecting integer number of clusters' %sys.argv[0], file=sys.stderr)
+            sys.exit(1)
+    if sys.argv[1].startswith('AGGLOMERATIVE') and len(sys.argv) < 4:
+        print('%s: expecting linkage type' %sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    elif sys.argv[1].startswith('AGGLOMERATIVE'):
+        if sys.argv[3] in ['average', 'complete']:
+            linkage = sys.argv[3]
+        else:
+            print('%s: invalid linkage type' %sys.argv[0], file=sys.stderr)
+            sys.exit(1)
+    elif len(sys.argv) > 3:
+        print('%s: unexpected trailing arguments' %sys.argv[0], file=sys.stderr)
+        sys.exit(1)
+    return model_type, n_clusters, linkage
+
+
+def read_movie_file():
+    try:
+        with open('../data/movies-large.json', 'r', encoding='utf-8') as f:
+            return json.load(f)
+    except (PermissionError, OSError) as e:
+        print(str(e), file=sys.stderr)
+        sys.exit(1)
+
+
+def get_movies_dict(all_movies, n_movies=1000):
+    """
+    # use a random subset of movies
+    # keys = random.sample(list(all_movies), n_movies)
+    keys = list(all_movies)
+    movies = {}
+    for key in keys:
+        movies[key] = all_movies[key]
+
+    return movies, len(keys)
+    """
+    return all_movies, len(list(all_movies))
+
 
 # split data into tokens delimited by commas
 def split(data):
     return re.split(r'\s*,\s*', data)
+
+
+def write_clusters(clusters, cluster_numbers):
+    if debug: print('Writing Clusters')
+    try:
+        with open('clusters.pk', 'wb') as f:
+            pickle.dump(clusters, f)
+        with open('cluster-numbers.pk', 'wb') as f:
+            pickle.dump(cluster_numbers, f)
+    except (PermissionError, OSError) as e:
+        print(str(e), file=sys.stderr)
+        sys.exit(1)
+
+
+def write_metrics(model_name, model, labels, clusters=0, width=35):
+    # Write evaluation metrics to a file, while preserving stats of other models
+    # We also preserve stats of the same model if it uses a different number of clusters
+    try:
+        with open('metrics.txt', 'r', encoding='utf-8') as f:
+            data = f.readlines()
+    except FileNotFoundError:
+        with open('metrics.txt', 'w', encoding='utf-8') as f:
+            data = ["{:<{width}} {:<{width}} {:<{width}} {:<{width}}\n".format("Model Name", "# Clusters",
+                                                                             "Silhouette Score", "Calinski-Hara Score",
+                                                                             width=width)]
+            f.write(data[0])
+
+
+    line_to_write = len(data)
+    for count, line in enumerate(data):
+        lline = line.split()
+        if lline[0] == model_name and lline[1] == str(clusters):
+            line_to_write = count
+            break
+
+    # These are heuristics to tell us how "good" our clustering is, how similar things in the cluster are
+    # We want to maximise these without getting our clusters so high that they become too small
+    silhouette_score = sklearn.metrics.silhouette_score(model, labels)
+    calinski_hara_score = sklearn.metrics.calinski_harabaz_score(model, labels)
+
+    if line_to_write == len(data):
+        data.append("{:<{width}} {:<{width}} {:<{width}} {:<{width}}\n".format(
+                model_name, clusters, silhouette_score, calinski_hara_score, width=width))
+    else:
+        data[line_to_write] = "{:<{width}} {:<{width}} {:<{width}} {:<{width}}\n".format(
+                model_name, clusters, silhouette_score, calinski_hara_score, width=width)
+
+    with open('metrics.txt', 'w', encoding='utf-8') as f:
+        f.writelines(data)
 
 
 if __name__ == '__main__':
