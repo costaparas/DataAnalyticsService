778fe88933e6253893d301010daf4572820e3f7b
Ben Liew <ben.liew.99@gmail.com>
2018-10-16 15:32:55 +1100

    Renamed random endpoint file

--- api/api.py
+++ api/api.py
@@ -26,10 +26,12 @@ api.namespaces.clear()
 from resources.movie import api as movie_api
 from resources.recommendation import api as recom_api
 from resources.auth_endpoint import api as token_api
-
+from resources.random_endpoint import api as random_api
 api.add_namespace(movie_api)
 api.add_namespace(recom_api)
 api.add_namespace(token_api)
+api.add_namespace(random_api)
+
 
 
 def parse_cmd_line_args():
